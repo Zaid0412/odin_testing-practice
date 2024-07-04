ab48b6906beefca59f4deb1746d9b5d9c0e9003b
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2024-07-04 09:01:06 +0300

    Refactor analyzeArray()

--- code/reverseString.js
+++ code/reverseString.js
@@ -7,13 +7,13 @@ function reverseString(str) {
   // Splitting up all of the characters of the string into elements of an array
   let allLetters = str.split("");
 
-  let allLettersReverse = [];
+  let result = [];
 
   // Looping over allLetters and pushing them to allLettersReversed in reversed order
   for (let i = allLetters.length - 1; i > -1; i--) {
-    allLettersReverse.push(allLetters[i]);
+    result.push(allLetters[i]);
   }
 
   // Joining all of the elements of the array to make it a string
-  return allLettersReverse.join("");
+  return result.join("");
 }
